26a3d78d9deff18f0da65a9413f0f976cae505a2
Bandoozle <marcosuteja78@gmail.com>
2023-06-30 14:19:16 -0700

    css edit dikit

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -15,17 +15,18 @@ const expandNavbar = () => {
   sidebar.classList.remove('close');
 };
 
-
 searchBtn.addEventListener('click', expandNavbar);
 modeSwitch.addEventListener('click', () => {
   body.classList.toggle('dark');
+  const isDarkModeEnabled = body.classList.contains('dark');
+  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
 
-  if (body.classList.contains('dark')) {
+  if (isDarkModeEnabled) {
     modeText.innerText = 'Light mode';
-    logo.src = '/img/logo2.png'; 
+    logo.src = '/img/logo2.png';
   } else {
     modeText.innerText = 'Dark mode';
-    logo.src = '/img/logo1.png'; 
+    logo.src = '/img/logo1.png';
   }
 });
 
@@ -37,20 +38,42 @@ document.addEventListener('click', (event) => {
   }
 });
 
-const wrapper = document.querySelector(".wrapper"),
-header = wrapper.querySelector("header");
-function onDrag({movementX, movementY}){
+const wrapper = document.querySelector('.wrapper');
+const header = wrapper.querySelector('header');
+
+function onDrag({ movementX, movementY }) {
   let getStyle = window.getComputedStyle(wrapper);
   let leftVal = parseInt(getStyle.left);
   let topVal = parseInt(getStyle.top);
   wrapper.style.left = `${leftVal + movementX}px`;
   wrapper.style.top = `${topVal + movementY}px`;
 }
-header.addEventListener("mousedown", ()=>{
-  header.classList.add("active");
-  header.addEventListener("mousemove", onDrag);
+
+header.addEventListener('mousedown', () => {
+  header.classList.add('active');
+  header.addEventListener('mousemove', onDrag);
 });
-document.addEventListener("mouseup", ()=>{
-  header.classList.remove("active");
-  header.removeEventListener("mousemove", onDrag);
+
+document.addEventListener('mouseup', () => {
+  header.classList.remove('active');
+  header.removeEventListener('mousemove', onDrag);
+});
+function applyTheme(isDarkMode) {
+  if (isDarkMode) {
+    body.classList.add('dark');
+    modeText.innerText = 'Light mode';
+    logo.src = '/img/logo2.png';
+  } else {
+    modeText.innerText = 'Dark mode';
+    logo.src = '/img/logo1.png';
+  }
+}
+
+const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+
+applyTheme(isDarkModeEnabled);
+
+modeSwitch.addEventListener('click', () => {
+  const isDarkModeEnabled = body.classList.contains('dark');
+  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
 });
