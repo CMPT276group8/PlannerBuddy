6574a52d36e89fc8b4c958649546bd4d338dbb08
Bandoozle <marcosuteja78@gmail.com>
2023-07-02 10:33:40 -0700

    login bekerja

---
+++ demo/src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.UsersModel;
+import com.example.demo.service.UsersService;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    private final UsersService  usersService;
+
+    public UsersController(UsersService usersService){
+        this.usersService = usersService;
+    }
+    
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("registerRequest", new UsersModel());
+        return "register_page"; //registerPage 
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model){
+        model.addAttribute("loginRequest", new UsersModel());
+        return "login_page"; //loginPage 
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute UsersModel usersModel){
+        System.out.println("register request:" + usersModel);
+        UsersModel registeredUser = usersService.registerUser(usersModel.getUsername(), usersModel.getPassword());
+        return registeredUser == null ? "register_page" : "redirect:/login"; //penyusup
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute UsersModel usersModel){
+        System.out.println("login request:" + usersModel);
+        UsersModel authenticated = usersService.authenticate(usersModel.getUsername(), usersModel.getPassword());
+        if(authenticated != null){
+            //model.addAttribute("userLogin", authenticated.getUsername()); //need a new Model model if implement this
+            return "main_page";
+        }else{
+            return "login_page";
+        }
+    }
+}
