f227aa9b7f80182e85af819f49f11d6b5ab69176
VerrillAng <verrill.ang@gmail.com>
2023-07-18 20:19:56 -0700

    add test for checkPassword() in DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.UsersModel;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +14,12 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	//test for checkPassword() function
+	@Test
+	void checkPassowrdTest() {
+		UsersModel user = new UsersModel();
+		user.setPassword("123");
+		user.setPasswordConfirm("123");
+		assertEquals(true, user.checkPassword());
+	}
 }
