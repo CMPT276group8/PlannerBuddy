b9b8f8319ada53027c6f9a7dc05555e25cc3879e
kevinss09 <kevinsugeng090@gmail.com>
2023-07-20 15:24:39 -0700

    Fix input checkbox and Add Icon

--- demo/src/main/resources/static/js/script.js
+++ demo/src/main/resources/static/js/script.js
@@ -1,153 +1,170 @@
-const body = document.querySelector('body');
-const sidebar = body.querySelector('nav');
-const toggle = body.querySelector('.toggle');
-const modeSwitch = body.querySelector('.toggle-switch');
-const modeText = body.querySelector('.mode-text');
-const logo = body.querySelector('#logo');
+const body = document.querySelector("body");
+const sidebar = body.querySelector("nav");
+const toggle = body.querySelector(".toggle");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-text");
+const logo = body.querySelector("#logo");
 
 const closeNavbar = () => {
-  sidebar.classList.add('close');
+	sidebar.classList.add("close");
 };
 
 const expandNavbar = () => {
-  sidebar.classList.remove('close');
+	sidebar.classList.remove("close");
 };
 
-modeSwitch.addEventListener('click', () => {
-  body.classList.toggle('dark');
-  const isDarkModeEnabled = body.classList.contains('dark');
-  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
+modeSwitch.addEventListener("click", () => {
+	body.classList.toggle("dark");
+	const isDarkModeEnabled = body.classList.contains("dark");
+	localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
 
-  if (isDarkModeEnabled) {
-    modeText.innerText = 'Light mode';
-    logo.src = '/img/logo2.png';
-  } else {
-    modeText.innerText = 'Dark mode';
-    logo.src = '/img/logo1.png';
-  }
+	if (isDarkModeEnabled) {
+		modeText.innerText = "Light mode";
+		logo.src = "/img/logo2.png";
+		document.querySelectorAll(".icon").forEach((element) => {
+			element.classList.add("white-theme");
+		});
+	} else {
+		modeText.innerText = "Dark mode";
+		logo.src = "/img/logo1.png";
+		document.querySelectorAll(".icon").forEach((element) => {
+			element.classList.remove("white-theme");
+		});
+	}
 });
 
-document.addEventListener('click', (event) => {
-  if (!sidebar.contains(event.target) && event.target !== toggle) {
-    closeNavbar();
-  } else if (sidebar.contains(event.target) && event.target !== toggle) {
-    expandNavbar();
-  }
+document.addEventListener("click", (event) => {
+	if (!sidebar.contains(event.target) && event.target !== toggle) {
+		closeNavbar();
+	} else if (sidebar.contains(event.target) && event.target !== toggle) {
+		expandNavbar();
+	}
 });
 
 function applyTheme(isDarkMode) {
-  if (isDarkMode) {
-    body.classList.add('dark');
-    modeText.innerText = 'Light mode';
-    logo.src = '/img/logo2.png';
-  } else {
-    modeText.innerText = 'Dark mode';
-    logo.src = '/img/logo1.png';
-  }
+	if (isDarkMode) {
+		body.classList.add("dark");
+		modeText.innerText = "Light mode";
+		logo.src = "/img/logo2.png";
+		document.querySelectorAll(".icon").forEach((element) => {
+			element.classList.add("white-theme");
+		});
+	} else {
+		modeText.innerText = "Dark mode";
+		logo.src = "/img/logo1.png";
+		document.querySelectorAll(".icon").forEach((element) => {
+			element.classList.remove("white-theme");
+		});
+	}
 }
 
-const isDarkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+const isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
 
 applyTheme(isDarkModeEnabled);
 
-modeSwitch.addEventListener('click', () => {
-  const isDarkModeEnabled = body.classList.contains('dark');
-  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
+modeSwitch.addEventListener("click", () => {
+	const isDarkModeEnabled = body.classList.contains("dark");
+	localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
 });
 
 // Function to close the alert
 function closeAlert() {
-  var alert = document.querySelector('.alert');
-  alert.style.display = 'none';
+	var alert = document.querySelector(".alert");
+	alert.style.display = "none";
 }
 
 // Admin Page
 function deleteSomething(id) {
-  fetch('/users/delete/' + id, {
-    method: 'DELETE'
-  })
-    .then(() => {
-      window.location.reload();
-    });
+	fetch("/users/delete/" + id, {
+		method: "DELETE",
+	}).then(() => {
+		window.location.reload();
+	});
 }
 
-
 // Tasklist Date
-const todayDateElement = document.getElementById('today-date');
-const tomorrowDateElement = document.getElementById('tomorrow-date');
+const todayDateElement = document.getElementById("today-date");
+const tomorrowDateElement = document.getElementById("tomorrow-date");
 
 // Get today's date
 const today = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const todayFormatted = today.toLocaleDateString('en-US', options);
+const options = {
+	weekday: "long",
+	year: "numeric",
+	month: "long",
+	day: "numeric",
+};
+const todayFormatted = today.toLocaleDateString("en-US", options);
 
 // Get tomorrow's date
 const tomorrow = new Date(today);
 tomorrow.setDate(today.getDate() + 1);
-const tomorrowFormatted = tomorrow.toLocaleDateString('en-US', options);
+const tomorrowFormatted = tomorrow.toLocaleDateString("en-US", options);
 
 // Update the elements with today's and tomorrow's dates
 todayDateElement.textContent = todayFormatted;
 tomorrowDateElement.textContent = tomorrowFormatted;
 
 function addTaskToList(listNumber, taskInput) {
-  const listContainer = document.getElementById('list-container-' + listNumber);
+	const listContainer = document.getElementById("list-container-" + listNumber);
 
-  let li = document.createElement('li');
-  li.addEventListener('click', toggleTask);
+	let li = document.createElement("li");
+	li.addEventListener("click", toggleTask);
 
-  // Create a div to contain task text and delete button
-  let taskContainer = document.createElement('div');
-  taskContainer.className = 'task-container';
+	// Create a div to contain task text and delete button
+	let taskContainer = document.createElement("div");
+	taskContainer.className = "task-container";
 
-  // Create a span for task text
-  let taskText = document.createElement('span');
-  let inputValue = taskInput.value.trim(); // Trim any leading/trailing whitespace
+	// Create a span for task text
+	let taskText = document.createElement("span");
+	let inputValue = taskInput.value.trim(); // Trim any leading/trailing whitespace
 
-  // Check if the task input is empty
-  if (inputValue === '') {
-    alert('Please enter a task.');
-    return; // Exit the function if the task input is empty
-  }
+	// Check if the task input is empty
+	if (inputValue === "") {
+		alert("Please enter a task.");
+		return; // Exit the function if the task input is empty
+	}
 
-  // Check if the task input exceeds the maximum character limit (e.g., 50 characters)
-  const maxCharacterLimit = 75;
-  if (inputValue.length > maxCharacterLimit) {
-    alert('Task input exceeds the maximum character limit of ' + maxCharacterLimit + ' characters.');
-    return; // Exit the function if the task input exceeds the character limit
-  }
+	// Check if the task input exceeds the maximum character limit (e.g., 50 characters)
+	const maxCharacterLimit = 75;
+	if (inputValue.length > maxCharacterLimit) {
+		alert(
+			"Task input exceeds the maximum character limit of " +
+				maxCharacterLimit +
+				" characters."
+		);
+		return; // Exit the function if the task input exceeds the character limit
+	}
 
-  taskText.innerHTML = inputValue;
-  taskText.className = 'task-text';
-  taskContainer.appendChild(taskText);
+	taskText.innerHTML = inputValue;
+	taskText.className = "task-text";
+	taskContainer.appendChild(taskText);
 
-  // Add delete button to the task container
-  let deleteButton = document.createElement('span');
-  deleteButton.innerHTML = 'X';
-  deleteButton.className = 'delete-button';
-  taskContainer.appendChild(deleteButton);
+	// Add delete button to the task container
+	let deleteButton = document.createElement("span");
+	deleteButton.innerHTML = "X";
+	deleteButton.className = "delete-button";
+	taskContainer.appendChild(deleteButton);
 
-  li.appendChild(taskContainer);
-  listContainer.appendChild(li);
+	li.appendChild(taskContainer);
+	listContainer.appendChild(li);
 
-  // Clear the task input field
-  taskInput.value = '';
+	// Clear the task input field
+	taskInput.value = "";
 }
 
 function toggleTask(e) {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('checked');
-  } else if (e.target.className === 'delete-button') {
-    e.target.parentElement.parentElement.remove();
-  }
+	if (e.target.tagName === "LI") {
+		e.target.classList.toggle("checked");
+	} else if (e.target.className === "delete-button") {
+		e.target.parentElement.parentElement.remove();
+	}
 }
 
 function deleteTodo(uid) {
-  fetch('/todo/delete/' + uid, {
-    method: 'DELETE'
-  })
-    .then(() => {
-      window.location.reload();
-    });
+	fetch("/todo/delete/" + uid, {
+		method: "DELETE",
+	}).then(() => {
+		window.location.reload();
+	});
 }
-
